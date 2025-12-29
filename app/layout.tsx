f6e05013d3f9718d1facc3ce8c58565202f5bb14
ktruogn05 <ktruogn123@gmail.com>
2025-12-29 17:58:07 +0700

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
 
-const geistSans = Geist({
+const font = Nunito({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -23,12 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={font.className}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
